cfe2b67b48619d1d5f9e2f209719d4932f8cd354
Hexi1997 <18715156450@163.com>
2024-09-20 00:27:59 +0800

    fix: build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-import { BasicUsage } from "./components/1_BasicUsage";
-import { Timeline } from "./components/2_Timeline";
-import { ScrollTriggerDemo } from "./components/3_ScrollTriggerDemo";
-import { ScrollTriggerSwiper } from "./components/4_ScrollTriggerSwiper";
-import { SVGAnimation } from "./components/5_SVGAnimation";
-import { SVGPathAnimation } from "./components/6_SVGPathAnimation";
-import { ClippathDemo } from "./components/7_ClipPath";
 import { Total } from "./components/Total";
 
 gsap.registerPlugin(ScrollTrigger);
